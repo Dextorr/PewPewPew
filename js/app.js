7f780900a15468712a73dfb5b9015c23626cb7a7
Dexter De Leon <dexdeleon@gmail.com>
2019-01-03 17:37:37 +0000

    Spacebar successfully firing shots that travel to the top of the grid.

--- js/app.js
+++ js/app.js
@@ -20,6 +20,21 @@ $(() => {
     $cells.eq(shipIndex).addClass('ship')
   }
 
+  function fireShot(){
+    let shotIndex = shipIndex
+    const shotTimer = setInterval(() => {
+      $cells.eq(shotIndex - 10).addClass('shot')
+      $cells.eq(shotIndex).removeClass('shot')
+      shotIndex -= 10
+      if (shotIndex<0){
+        clearInterval(shotTimer)
+        $cells.eq(shotIndex).removeClass('shot')
+      }
+      console.log('shot', shotIndex)
+    }, 200)
+    console.log('Shots fired!')
+  }
+
   $(document).on('keydown', e => {
     console.log(e.keyCode)
     $cells.eq(shipIndex).removeClass('ship')
@@ -27,6 +42,8 @@ $(() => {
       case 37: if (shipIndex%10 !== 0) shipIndex--
         break
       case 39: if (shipIndex%10 !== 9) shipIndex++
+        break
+      case 32: fireShot()
         break
     }
     moveShip()
