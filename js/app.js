f27bc3c987a7aecea3500fad261f888ca6f6fb56
Dexter De Leon <dexdeleon@gmail.com>
2019-01-03 15:56:02 +0000

    JS creating grid of 100. Ship placed on bottom row and can move left and right on bottom row with arrow keys.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,35 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const h1 = document.querySelector('h1')
-  h1.textContent = 'Hello World!'
+$(() => {
+  const $gameBoard = $('#gameBoard')
+  let shipIndex = 94
+
+
+  $gameBoard.css({border: '5px solid #000'})
+
+  for (let i=0;i<100;i++){
+    const $cell = $('<div />')
+    $cell.addClass('cell')
+      .appendTo($gameBoard)
+  }
+
+  const $cells = $('div.cell')
+
+  $cells.eq(shipIndex)
+    .addClass('ship')
+
+  function moveShip(){
+    $cells.eq(shipIndex).addClass('ship')
+  }
+
+  $(document).on('keydown', e => {
+    console.log(e.keyCode)
+    $cells.eq(shipIndex).removeClass('ship')
+    switch(e.keyCode){
+      case 37: if (shipIndex%10 !== 0) shipIndex--
+        break
+      case 39: if (shipIndex%10 !== 9) shipIndex++
+        break
+    }
+    moveShip()
+    console.log(shipIndex)
+  })
 })
