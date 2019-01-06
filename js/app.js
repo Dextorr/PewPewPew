e606021c42cb982e7a3cf4f030fa5e76b772f9c1
Dexter De Leon <dexdeleon@gmail.com>
2019-01-06 22:07:20 +0000

    Refactored aliens to be objects held in an array handled by the same interval.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 $(() => {
 
-  const $gameBoard = $('#gameBoard')
-  let shipIndex = 94,
-    alienMoveTimer
+  const $gameBoard = $('#gameBoard'),
+    aliens = []
+  let shipIndex = 94
 
   // TEMP just to see gameboard clearly for now
   $gameBoard.css({border: '5px solid #000'})
@@ -18,75 +18,82 @@ $(() => {
   const $cells = $('div.cell')
 
   // Place player ship on initial cell (94)
-  $cells.eq(shipIndex)
-    .addClass('ship')
+  $cells.eq(shipIndex).addClass('ship')
 
   // Redraws the ship on the cell the player has moved it to
   function moveShip(){
     $cells.eq(shipIndex).addClass('ship')
   }
 
+  function isAlien(index){
+    return $cells.eq(index).hasClass('alien')
+  }
+
   // Randomly places an alien on one of the first 9 cells of the first row...
   function spawnAlien(){
     const alienIndex = Math.floor(Math.random() * 9)
-    if (!$cells.eq(alienIndex).hasClass('alien')){
-      $cells.eq(alienIndex).addClass('alien')
+    //Check that the area is clear for an alien to spawn and move into
+    if (!(isAlien(alienIndex) || isAlien(alienIndex + 1) || isAlien(alienIndex +2))){
+      drawAlien(alienIndex, 0)
+      const alien = {
+        'index': alienIndex,
+        'dir': 'right',
+        'shotOdds': 0.75
+      }
+      aliens.push(alien)
+      console.log(aliens)
     }
-    // ...and applies movement logic to it
-    moveAlien(alienIndex)
   }
 
   // Draws the alien on the next cell (n) and removes it from the current one (index)
   function drawAlien(index, n){
-    $cells.eq(index + n).addClass('alien')
     $cells.eq(index).removeClass('alien')
+    $cells.eq(index + n).addClass('alien')
   }
 
   // Alien randomly fires a shot
-  function alienShot(alienIndex){
+  function alienShot(alien){
     const shotCheck = Math.random()
-    if (shotCheck > 0.75){
-      fireShot(alienIndex, 10)
+    if (shotCheck > alien.shotOdds && !$cells.eq(alien.index + 10).hasClass('alien')){
+      fireShot(alien.index, 10)
     }
   }
 
-  // Alien movement logic:
-  function moveAlien(alienIndex){
+  // Alien ship moves every half second
+  setInterval(() => {
     // Initial direction is right
-    let dir = 'right'
-    // Alien ship moves every half second
-    alienMoveTimer = setInterval(() => {
+    aliens.forEach(alien => {
       // When the alien reaches the right edge...
-      if (alienIndex%10 === 9 && Math.ceil((alienIndex/10)%2) === 1){
+      if (alien.index%10 === 9 && Math.ceil((alien.index/10)%2) === 1){
         // ...it moves down a row...
-        drawAlien(alienIndex, 10)
-        alienIndex += 10
+        drawAlien(alien.index, 10)
+        alien.index += 10
         // ...and reverses direction to left
-        dir = 'left'
-      // When the alien reaches the left edge...
-      } else if (alienIndex%10 === 0 && (alienIndex/10)%2 === 1){
+        alien.dir = 'left'
+        // When the alien reaches the left edge...
+      } else if (alien.index%10 === 0 && (alien.index/10)%2 === 1){
         // ...it moves down a row...
-        drawAlien(alienIndex, 10)
-        alienIndex += 10
+        drawAlien(alien.index, 10)
+        alien.index += 10
         // ...and reverses direction to right
-        dir = 'right'
-      // While direction is right...
-      } else if (dir === 'right'){
+        alien.dir = 'right'
+        // While direction is right...
+      } else if (alien.dir === 'right'){
         //...check if the alien will fire a shot...
-        alienShot(alienIndex)
+        alienShot(alien)
         // ...and the alien moves one cell right
-        drawAlien(alienIndex, 1)
-        alienIndex ++
-      // While direction is left...
-      } else if (dir === 'left'){
+        drawAlien(alien.index, 1)
+        alien.index ++
+        // While direction is left...
+      } else if (alien.dir === 'left'){
         //...check if the alien will fire a shot...
-        alienShot(alienIndex)
+        alienShot(alien)
         // ...and the alien moves one cell left
-        drawAlien(alienIndex, -1)
-        alienIndex --
+        drawAlien(alien.index, -1)
+        alien.index --
       }
-    }, 500)
-  }
+    })
+  }, 500)
 
   // Fires a shot from the player ship's current position:
   function fireShot(shooterIndex, dir){
@@ -100,13 +107,17 @@ $(() => {
       $cells.eq(shotIndex).removeClass('shot')
       // ...and current position is reassigned to new position
       shotIndex += dir
+
       // Collision detection
       if($cells.eq(shotIndex).hasClass('alien')){
         $cells.eq(shotIndex).removeClass('alien')
         $cells.eq(shotIndex).removeClass('shot')
-        clearInterval(alienMoveTimer)
+        console.log(aliens.find(alien => alien.index === shotIndex))
+        aliens.splice(aliens.find((alien) => alien.index === shotIndex), 1)
+        console.log(aliens, shotIndex)
         clearInterval(shotTimer)
       }
+
       // When the shot has reached the top of the screen...
       if (shotIndex<0 || shotIndex>100){
         // ...the movement timer stops...
